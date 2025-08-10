05766208d597033abf5ed2a37dc8e6a21edc1c0e
Karl Adams <109367281+Five50@users.noreply.github.com>
2025-08-10 14:30:58 -0400

    First Build Commit
    Initial plugin files and gitignore

---
+++ brag-book-gallery.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * BRAG Book Plugin
+ *
+ * This file is used to generate all plugin information. Including all the
+ * dependencies used by the plugin, registers the activation and deactivation
+ * functions, and defines a function that starts the plugin.
+ *
+ * @author     Candace Crowe Design <bragbook@candacecrowe.com>
+ * @copyright  Copyright (c) 2025, Candace Crowe Design LLC
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @link       https://www.bragbookgallery.com/
+ * @package    BRAGBook
+ * @since      3.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:       BRAGBook Gallery
+ * Plugin URI:        https://www.bragbookgallery.com/
+ * Description:       Installs necessary components to display your BRAGbook
+ *                    before and after gallery.
+ * Version:           3.0.0
+ * Requires at Least: 6.8
+ * Requires PHP:      8.2
+ * Author:            Candace Crowe Design <bragbook@candacecrowe.com>
+ * Author URI:        https://www.bragbookgallery.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       brag-book-gallery
+ */
+
+namespace BRAGBookGallery;
